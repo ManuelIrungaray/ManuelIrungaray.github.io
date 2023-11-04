9fc736150d91b5f2c4eadaeff129cdae5c42fdb4
Manuel I <112410801+ManuelIrungaray@users.noreply.github.com>
2023-11-03 23:19:03 -0500

    Delete frontend/js/sharing.js

--- frontend/js/sharing.js
+++
@@ -1,6 +0,0 @@
-var button = document.getElementById("my-button");
-button.addEventListener("click", changeColor);
-
-function changeColor() {
-  document.getElementById("my-div").style.backgroundColor = "green";
-}
