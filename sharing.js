3815c10f64a47618efd2f1f32b0bd9b97def7b6e
Manuel I <112410801+ManuelIrungaray@users.noreply.github.com>
2023-11-04 00:50:37 -0500

    Update sharing.js

--- sharing.js
+++ sharing.js
@@ -1,5 +1,10 @@
 var button = document.getElementById("my-button");
-button.addEventListener("click", changeColor);
+//button.addEventListener("click", changeColor);
+
+button.addEventListener("click", function(event) {
+    event.preventDefault();
+    popupWindow.style.display = "block";
+  });
 
 function changeColor() {
   document.getElementById("my-div").style.backgroundColor = "green";
